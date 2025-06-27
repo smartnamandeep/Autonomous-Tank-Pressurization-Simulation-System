c06ece4b9b1211cbc7f3366fd51bb8a89e1977ee
smartnamandeep <85362196+smartnamandeep@users.noreply.github.com>
2025-06-27 15:16:41 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+#imports
+import numpy 
+
+######## Constants ########
+
+lox_tank_length = 4.55 # meters
+rp_tank_length = 8.45 # meters
+mass_max_lox = 18956 # kg
+mass_max_rp = 21986 # kg
+vol_lox_tank = 18.435 # Cubic meter
+vol_rp_tank = 26.039 # Cubic meter
+mass_lox = 0 # kg
+mass_rp = 0 # kg
+vol_lox = 0 # Cubic meter
+vol_rp = 0 # Cubic meter
+boiloff_rate =  
+percent_lox = 0 # %, how much of lox tank is filled up 
+percent_rp = 0 # %, how much of rp-1 tank is filled up 
+surfaceAreaInContact_lox = # how much surface is lox physically touching
+surfaceAreaInContact_rp =  # how much surface is rp-1 physically touching
+
+
+####### formulas #######
+
+# PV = nRT  Ideal gas law
+
+
